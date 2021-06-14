f8ef29054c2f223bf199fe127c44275a123c9451
iamsantosh <sththapa444@gmail.com>
2021-06-14 11:16:55 +0545

    Reactjs UI clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Row from './Row'
+import requests from './request'
+import Banner from './Banner';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Row title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+      <Row title="ACTION" fetchUrl={requests.fetchActionMovies} />
+      <Row title="COMEDY" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HORROR" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOCUMENTARY" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
